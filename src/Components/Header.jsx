564ac29d83e5b19aa8f8526d2e33601d9d4e71cc
Luiz Claudio <luizclaudioarruda@outlook.com>
2023-12-21 23:00:07 +0000

    modified:   .eslintrc modified:   src/Components/Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,12 @@
-/**
- * Header Component
- *
- * Sample of a single responsibility component
- */
-
 import React from "react";
+import PropTypes from "prop-types";
 
-const Header = () => {
-  return <h1>Educator React Codespaces JS Template</h1>;
+const Header = ({ title }) => {
+  return <h1>Educator React Codespaces JS Template - {title}</h1>;
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
 };
 
 export default Header;
